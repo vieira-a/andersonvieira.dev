290bb784fbf0aabf156f7591e7f3b3fe3f62bc2c
Anderson Vieira <asvieira.br@gmail.com>
2023-12-06 01:00:43 -0300

    feat: add input generic component

---
+++ src/app/ui/input.tsx
@@ -0,0 +1,14 @@
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  type: string;
+  placeholder: string;
+}
+
+export default function Input({ type, placeholder }: InputProps ) {
+  return (
+    <input 
+      className='w-full px-3 py-5 outline-none rounded-xl border-[1px] border-gray-300'
+      type={type} 
+      placeholder={placeholder} 
+    />
+  )
+}
