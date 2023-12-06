4624b2688d5ad4125f664a9bd19a2d6fd814f4fd
Anderson Vieira <asvieira.br@gmail.com>
2023-12-06 01:00:52 -0300

    feat: add text area generic component

---
+++ src/app/ui/text-area.tsx
@@ -0,0 +1,13 @@
+interface TextAreaProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  placeholder: string;
+}
+
+export default function TextArea({ placeholder }: TextAreaProps ) {
+  return (
+    <textarea 
+      rows={6}
+      className='w-full px-3 py-5 outline-none rounded-xl border-[1px] border-gray-300' 
+      placeholder={placeholder} 
+    />
+  )
+}
